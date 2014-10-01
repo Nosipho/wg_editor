3d41aa88cf263249ff8595c2f8e4bbdf572ab208
Colin Manning <colin.manning@mac.com>
2014-10-01 16:15:52 +0200

    Initial validation, some more fields, add workgroup started

--- js/controllers.js
+++ js/controllers.js
@@ -12,6 +12,7 @@ editorControllers.controller('editorController', function($scope, pf3service, da
     $scope.dirtyUser = false;
     $scope.selectedUser = {};
     $scope.currentUser = {};
+    $scope.workgroups = [];
 
     $scope.clipBoard = {
         workgroup: {},
@@ -20,12 +21,12 @@ editorControllers.controller('editorController', function($scope, pf3service, da
     }
 
     pf3service.getWorkgroups().success(function (response) {
-        $scope.workgroups =  response;
+        $scope.workgroups = response;
     }).then(function (message) {
         $scope.selectWorkgroup($scope.workgroups[0]);
     });
 
-    $scope.selectWorkgroup = function(workgroup) {
+    $scope.selectWorkgroup = function (workgroup) {
         $scope.selectedWorkgroup = workgroup;
         pf3service.getWorkgroupConfig($scope.selectedWorkgroup.id).success(function (response) {
             $scope.currentWorkgroup = response;
@@ -33,55 +34,135 @@ editorControllers.controller('editorController', function($scope, pf3service, da
         });
     }
 
-    $scope.selectClient = function(client) {
+    $scope.selectClient = function (client) {
         $scope.selectedClient = client;
         $scope.currentClient = client;
         $scope.selectUser(_.values($scope.currentClient.users)[0]);
     }
 
-    $scope.selectUser = function(user) {
+    $scope.selectUser = function (user) {
         $scope.selectedUser = user;
         $scope.currentUser = user;
     }
 
-    $scope.userSetFullName = function() {
+    $scope.userSetFullName = function () {
         $scope.currentUser.full_name = $scope.currentUser.first_name + " " + $scope.currentUser.last_name;
     }
 
-    $scope.saveCurrentUser = function() {
+    $scope.saveCurrentUser = function () {
         $scope.currentClient.users[$scope.currentUser.id] = $scope.currentUser;
     }
 
-    $scope.saveCurrentClient = function() {
+    $scope.saveCurrentClient = function () {
         $scope.currentWorkgroup.clients[$scope.currentClient.name] = $scope.currentClient;
     }
 
-    $scope.saveCurrentWorkgroup = function() {
+    $scope.saveCurrentWorkgroup = function () {
         // TODO save workgroup
     }
 
-    $scope.addUser = function() {
+    $scope.addUser = function () {
         $scope.currentUser = {
             first_name: "New",
             last_name: "User",
             full_name: "New User",
             id: "0000",
-            email: "new.user@printoutsource.com"
+            email: "new.user@printoutsource.com",
+            dropbox_folder: "New User"
         }
     }
 
-    $scope.copyUser = function() {
+    $scope.copyUser = function () {
         $scope.clipBoard.user = $scope.currentUser;
     }
 
-    $scope.pasteUser = function() {
+    $scope.pasteUser = function () {
         $scope.currentUser = $scope.clipBoard.user;
     }
 
-    $scope.deleteUser = function() {
+    $scope.deleteUser = function () {
         if ($scope.currentClient.users[$scope.currentUser.id]) {
             delete $scope.currentClient.users[$scope.currentUser.id];
             $scope.selectClient($scope.currentClient);
+        }
+    }
+
+    $scope.addClient = function () {
+        var client = $scope.dummyClient("New Client");
+        $scope.selectClient(client);
+    }
+
+    $scope.selectDummyWorkgroup = function (briefWorkgroup, workgroup) {
+        $scope.selectedWorkgroup = briefWorkgroup;
+        $scope.currentWorkgroup = workgroup;
+        $scope.selectClient(_.values($scope.currentWorkgroup.clients)[0]);
+    }
+
+    $scope.addWorkgroup = function () {
+        var workgroup = $scope.dummyWorkgroup();
+        var briefWorkgroup = {
+            id: workgroup.id,
+            name: workgroup.name
+        }
+        $scope.workgroups.push(workgroup);
+        $scope.selectDummyWorkgroup(briefWorkgroup, workgroup);
+
+    }
+
+    $scope.dummyWorkgroup = function() {
+        var client = $scope.dummyClient("New Workgroup");
+        return {
+            id: 0,
+            name: "New Workgroup",
+            os_group_name: "printflow",
+            id_parent: 0,
+            class_parent: "None",
+            preflight_profile: "/opt/printflow2/callas/myprofiles/kishan_profile_10.kfpx",
+            os_userid: 500,
+            os_groupid: 500,
+            os_user_name: "printflow",
+            dam_site: "new-workgroup",
+            class: "workgroup",
+            dropbox_root: "/home/printflow/Dropbox/pf_new-workgroup",
+            serial_number: "PF2001",
+            clients: { "New Workgroup": client }
+        }
+
+    }
+
+    $scope.dummyClient = function(name) {
+        var user = $scope.dummyUser("1001");
+        return {
+            name: name,
+            users: {
+                "1001": user
+            },
+            client_emails: [],
+            buyer_emails: [],
+            dam_category_id: "",
+            file_notify_emails: [
+                "printflow@printoutsource.com",
+            ],
+            "noosh_nge_enabled": false,
+            "agency_emails": [],
+            "account_managers": [
+                "1001"
+            ],
+            "account_manager_emails": [
+            ],
+            "project_notify_emails": [
+            ]
+        };
+    }
+
+    $scope.dummyUser = function(id) {
+        return {
+            first_name: "New",
+            last_name: "User",
+            full_name: "New User",
+            id: id,
+            email: "new.user@printoutsource.com",
+            dropbox_folder: "New User"
         }
     }
 
